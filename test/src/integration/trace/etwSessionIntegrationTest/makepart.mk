b359ca3da5ebb03b50c85d6888c31c7ef81b5361
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-29 19:58:42 +0900

    ビルドエラー対処

--- test/src/integration/trace/etwSessionIntegrationTest/makepart.mk
+++ test/src/integration/trace/etwSessionIntegrationTest/makepart.mk
@@ -1,5 +1,6 @@
 # テスト対象のソースファイル
 TEST_SRCS := \
+    $(MYAPP_DIR)/prod/libsrc/com_util/sync/sync_windows.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw_session.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/crt/string.c
@@ -8,3 +9,6 @@ TEST_SRCS := \
 INCDIR += \
     $(MYAPP_DIR)/prod/libsrc/com_util/crt \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw
+
+# ライブラリの指定
+LIBS += mock_libc
