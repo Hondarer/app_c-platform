6194cce469f75c852e8b4f6ac4496c9526cad3e7
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-02 05:47:41 +0900

    階層構造整理

--- test/src/cmd/etw-viewerTest/makepart.mk
+++ test/src/cmd/etw-viewerTest/makepart.mk
@@ -1,6 +1,6 @@
 # テスト対象のソースファイル
 TEST_SRCS := \
-	$(MYAPP_DIR)/prod/src/etw-viewer/etw-viewer.c
+	$(MYAPP_DIR)/prod/src/cmd/etw-viewer/etw-viewer.c
 
 # エントリーポイントの変更
 # テスト対象のソースファイルにある main() は直接実行されず、
@@ -9,7 +9,7 @@ USE_WRAP_MAIN := 1
 
 # テスト対象ソースのローカルヘッダーを参照する
 INCDIR += \
-	$(MYAPP_DIR)/prod/src/etw-viewer
+	$(MYAPP_DIR)/prod/src/cmd/etw-viewer
 
 # ライブラリの指定
 LIBS += mock_com_util mock_libc
--- test/src/cmd/trace-cliTest/makepart.mk
+++ test/src/cmd/trace-cliTest/makepart.mk
@@ -1,6 +1,6 @@
 # テスト対象のソースファイル
 TEST_SRCS := \
-	$(MYAPP_DIR)/prod/src/trace-cli/trace-cli.c
+	$(MYAPP_DIR)/prod/src/cmd/trace-cli/trace-cli.c
 
 # エントリーポイントの変更
 # テスト対象のソースファイルにある main() は直接実行されず、
@@ -9,7 +9,7 @@ USE_WRAP_MAIN := 1
 
 # テスト対象ソースのローカルヘッダーを参照する
 INCDIR += \
-	$(MYAPP_DIR)/prod/src/trace-cli
+	$(MYAPP_DIR)/prod/src/cmd/trace-cli
 
 # ライブラリの指定
 LIBS += mock_com_util mock_libc
