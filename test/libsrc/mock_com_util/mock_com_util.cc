13e384e5589ba63302469b11908b2e4f1dcb9282
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-05 07:39:10 +0900

    モック整理

--- test/libsrc/mock_com_util/mock_com_util.cc
+++ test/libsrc/mock_com_util/mock_com_util.cc
@@ -1,5 +1,16 @@
+#include <com_util/base/platform.h>
 #include <testfw.h>
 #include <mock_com_util.h>
+
+#if defined(COMPILER_MSVC)
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vfprintf")
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vfopen_fmt")
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vaccess_fmt")
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vopen_fmt")
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vremove_fmt")
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vmkdir_fmt")
+#pragma comment(linker, "/INCLUDE:_mock_impl_com_util_vstat_fmt")
+#endif /* COMPILER_MSVC */
 
 Mock_com_util *_mock_com_util = nullptr;
 
