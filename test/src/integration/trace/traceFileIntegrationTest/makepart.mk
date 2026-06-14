b0dbfaa8a84916ed9c7aa57dda5dc0d3b85e39c9
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-06-14 19:26:41 +0900

    コンパイルエラー対策

--- test/src/integration/trace/traceFileIntegrationTest/makepart.mk
+++ test/src/integration/trace/traceFileIntegrationTest/makepart.mk
@@ -8,7 +8,8 @@ TEST_SRCS := \
     $(MYAPP_DIR)/prod/libsrc/com_util/crt/stdio.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/file/trace_file.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/syslog/trace_syslog.c \
-    $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw.c
+    $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw.c \
+    $(MYAPP_DIR)/test/libsrc/mock_com_util/trace/mock_com_util_eventlog_sink_dispose_on_unload.cc
 
 # ライブラリの指定
 LIBS += mock_libc com_util
