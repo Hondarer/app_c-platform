09351e4c6b3c18cfc1fb33d86a37c8ad7ff0dcce
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-07-08 05:57:39 +0900

    Windows ETW テストのリンク対象を修正
    sync_windows.c が参照する sync_descriptor.c を
    etwSessionIntegrationTest の TEST_SRCS に追加する。

--- test/src/integration/trace/etwSessionIntegrationTest/makepart.mk
+++ test/src/integration/trace/etwSessionIntegrationTest/makepart.mk
@@ -1,5 +1,6 @@
 # テスト対象のソース ファイル
 TEST_SRCS := \
+    $(MYAPP_DIR)/prod/libsrc/com_util/sync/sync_descriptor.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/sync/sync_windows.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw.c \
     $(MYAPP_DIR)/prod/libsrc/com_util/trace/backends/etw/trace_etw_session.c \
